922ca2dc74811a80ced13722c09f055003bd35ce
Bilalkhaa <bkhalid@poneahealth.com>
2022-10-17 06:45:07 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import sqlite3
+from flask import Flask, render_template, request, url_For, flash, redirect
+from werkzeug.exceptions import abort
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    return render_template('create.html')
+
+if __name__ == '__main__':
+    app.run(debug = True);
